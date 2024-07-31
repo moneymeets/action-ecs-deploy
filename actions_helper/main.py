9883719d8c2d2f361788ca4562aa7a4cdcbacb1e
Felix Hoffmann <felix.hoffmann@moneymeets.com>
2024-07-31 08:38:01 +0100

    feat(action): run migration task before updating service

--- actions_helper/main.py
+++ actions_helper/main.py
@@ -5,6 +5,7 @@ import click
 
 from actions_helper.commands.get_active_task_definition_by_tag import get_active_task_definition_arn_by_tag
 from actions_helper.commands.wait_for_service_stability import wait_for_service_stability
+from actions_helper.commands.wait_for_task_stopped import wait_for_task_stopped
 
 
 @click.group()
@@ -25,6 +26,22 @@ def cmd_wait_for_service_stability(
         ecs_client=boto3.Session(region_name=aws_region).client("ecs"),
         cluster=cluster,
         service=service,
+    )
+
+
+@cli.command(name="wait-for-task-stopped")
+@click.option("--aws-region", default=os.environ.get("AWS_DEFAULT_REGION"), type=str)
+@click.option("--cluster", type=str)
+@click.option("--task", type=str)
+def cmd_wait_for_task_stopped(
+    aws_region: str,
+    cluster: str,
+    task: str,
+):
+    wait_for_task_stopped(
+        ecs_client=boto3.Session(region_name=aws_region).client("ecs"),
+        cluster=cluster,
+        task=task,
     )
 
 
